9571b5eea0345b83054cd2793f69e7f71ae0e8e5
psiservices-wxavier <56822099+psiservices-wxavier@users.noreply.github.com>
2020-02-21 11:20:52 +0000

    Update vac2_-_basic_tests.test.js
    Fixed broken tests

--- Auto Test/VAC2/vac2_-_basic_tests.test.js
+++ Auto Test/VAC2/vac2_-_basic_tests.test.js
@@ -585,186 +585,9 @@
 
 
 
-"@test"["VAC2 - Edited email - Preview Doc - Remove"] = {
-       '0': function() {
-        var ref = window.document.getElementsByTagName("script")[0]; 
-        var script = window.document.createElement("script"); 
-        script.id="testStoreStateScript"; 
-        script.src ="/testStateChooser.js"; 
-        script.type="text/javascript"; 
-        ref.parentNode.insertBefore(script, ref);
-    },
-    '2.Click submit button "Use State"': function() {
-        var actionTarget = function() {
-            return $(":containsExcludeChildren(Use State)").eq(6);
-        };
-        act.click(actionTarget);
-    },
-    '3.Click div "(No Subject)"': function() {
-        var actionTarget = function() {
-            return $(":containsExcludeChildren(No Subject)").eq(1);
-        };
-        act.click(actionTarget);
-    },
-    '4.Click span "Draft - Diversity..."': function() {
-        act.click(":containsExcludeChildren(Draft Diversity of Delegates)");
-    },
-    '5.Click link "Preview"': function() {
-        act.click(":containsExcludeChildren(Preview)");
-    },
-    '6.Click button "Close"': function() {
-        var actionTarget = function() {
-            return $("#modal\\:ck6rxdp4d000f3b6rol9588nm").find(":containsExcludeChildren(Close)");
-        };
-        act.click(actionTarget);
-    },
-    '7.Click span "Draft - Diversity..."': function() {
-        act.click(":containsExcludeChildren(Draft Diversity of Delegates)");
-    },
-    '8.Click link "Remove"': function() {
-        act.click("[data-testid='removeLink'].dropdown-item");
-    },
-    '9.Click button "Yes, Remove"': function() {
-        act.click(":containsExcludeChildren(Yes Remove)");
-    }
-};
 
-"@test"["VAC2 - Document - Tool bar test"] = {
-       '0': function() {
-        var ref = window.document.getElementsByTagName("script")[0]; 
-        var script = window.document.createElement("script"); 
-        script.id="testStoreStateScript"; 
-        script.src ="/testStateChooser.js"; 
-        script.type="text/javascript"; 
-        ref.parentNode.insertBefore(script, ref);
-    },
-    '2.Click submit button "Use State"': function() {
-        var actionTarget = function() {
-            return $(":containsExcludeChildren(Use State)").eq(6);
-        };
-        act.click(actionTarget);
-    },
-    '2.Click span "Documents"': function() {
-        var actionTarget = function() {
-            return $("#root").find(":containsExcludeChildren(Documents)");
-        };
-        act.click(actionTarget);
-    },
-    '3.Click div "Test Document"': function() {
-        act.click(":containsExcludeChildren(Test Document)");
-    },
-    '4.Click span "Paragraph"': function() {
-        act.click("#ck-editor__aria-label_e22fbeae1d54c3606ed37a69fc9e153d9");
-    },
-    '5.Click span "Heading 1"': function() {
-        act.click("#ck-editor__aria-label_e008877f25458794ecbfba3571a2e7d7b");
-    },
-    "6.Click button": function() {
-        act.click(".ck.ck-button.ck-off.ck-button_with-text.ck-dropdown__button");
-    },
-    '7.Click span "Heading 2"': function() {
-        act.click("#ck-editor__aria-label_e2ae6517cfc019a9336ed6607d1089f19");
-    },
-    "8.Click button": function() {
-        act.click(".ck.ck-button.ck-off.ck-button_with-text.ck-dropdown__button");
-    },
-    '9.Click span "Heading 3"': function() {
-        act.click("#ck-editor__aria-label_e80414287f655c92148f2c3c1656287e6");
-    },
-    "10.Click button": function() {
-        act.click(".ck.ck-button.ck-off.ck-button_with-text.ck-dropdown__button");
-    },
-    "11.Click button": function() {
-        act.click(".ck.ck-button.ck-heading_paragraph.ck-off.ck-button_with-text");
-    },
-    "12.Click <path>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > button:nth(0) > svg:nth(0) > path:nth(0)");
-    },
-    "13.Click button": function() {
-        act.click(".ck.ck-button.ck-fontsize-option.text-big.ck-off.ck-button_with-text");
-    },
-    "14.Click <svg>": function() {
-        var actionTarget = function() {
-            return $(".ck.ck-button.ck-off.ck-dropdown__button").find(" > svg:nth(0)").eq(1);
-        };
-        act.click(actionTarget);
-    },
-    '15.Click span "Default"': function() {
-        act.click("#ck-editor__aria-label_ed6eedd5c32174e3026d62a047a61d9fc");
-    },
-    "16.Click <svg>": function() {
-        var actionTarget = function() {
-            return $(".ck.ck-button.ck-off.ck-dropdown__button").find(" > svg:nth(0)").eq(2);
-        };
-        act.click(actionTarget);
-    },
-    '17.Click span "Arial"': function() {
-        act.click("#ck-editor__aria-label_ef8e6df3c0c89972e43e9a020c7811dcf");
-    },
-    "18.Click button": function() {
-        var actionTarget = function() {
-            return $(".ck.ck-button.ck-off.ck-dropdown__button").eq(2);
-        };
-        act.click(actionTarget);
-    },
-    '19.Click span "Georgia"': function() {
-        act.click("#ck-editor__aria-label_eb1fcbddb322458f99a30d08773a40780");
-    },
-    "20.Click <path>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > button:nth(0) > svg:nth(0) > path:nth(0)");
-    },
-    "21.Click <svg>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > button:nth(1) > svg:nth(0)");
-    },
-    "22.Click <svg>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > button:nth(2) > svg:nth(0)");
-    },
-    "23.Click <svg>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > button:nth(3) > svg:nth(0)");
-    },
-    "24.Click <path>": function() {
-        var actionTarget = function() {
-            return $(".ck-icon__fill").eq(0);
-        };
-        act.click(actionTarget);
-    },
-    "25.Click <svg>": function() {
-        var actionTarget = function() {
-            return $(".ck.ck-button.ck-off.ck-dropdown__button").find(" > svg:nth(0)").eq(3);
-        };
-        act.click(actionTarget);
-    },
-    "26.Click <svg>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(4) > div:nth(0) > div:nth(0) > div:nth(0) > button:nth(1) > svg:nth(0)");
-    },
-    "27.Click <path>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(4) > button:nth(0) > svg:nth(0) > path:nth(0)");
-    },
-    "28.Click <svg>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(4) > div:nth(0) > div:nth(0) > div:nth(0) > button:nth(2) > svg:nth(0)");
-    },
-    "29.Click <path>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(4) > button:nth(0) > svg:nth(0) > path:nth(0)");
-    },
-    "30.Click <path>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(4) > div:nth(0) > div:nth(0) > div:nth(0) > button:nth(3) > svg:nth(0) > path:nth(0)");
-    },
-    "31.Click <path>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > button:nth(4) > svg:nth(0) > path:nth(0)");
-    },
-    "32.Click <svg>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > button:nth(5) > svg:nth(0)");
-    },
-    "33.Click <svg>": function() {
-        var actionTarget = function() {
-            return $(".ck.ck-button.ck-disabled.ck-off").find(" > svg:nth(0)").eq(0);
-        };
-        act.click(actionTarget);
-    },
-    "34.Click <svg>": function() {
-        act.click("body > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(1) > div:nth(2) > div:nth(0) > div:nth(1) > div:nth(1) > div:nth(0) > div:nth(0) > div:nth(0) > div:nth(0) > button:nth(7) > svg:nth(0)");
-    }
-};
+
+
 
 "@test"["VAC2 - Preview Document and Save - Open"] = {
        '0': function() {
@@ -822,6 +645,59 @@
     },
     '9.Click button "Save and Open"': function() {
         act.click(":containsExcludeChildren(Save and Open)");
+    }
+};
+
+"@test"["VAC2 - Edited email - Preview Doc - Remove"] = {
+      '0': function() {
+        var ref = window.document.getElementsByTagName("script")[0]; 
+        var script = window.document.createElement("script"); 
+        script.id="testStoreStateScript"; 
+        script.src ="/testStateChooser.js"; 
+        script.type="text/javascript"; 
+        ref.parentNode.insertBefore(script, ref);
+    },
+    '2.Click submit button "Use State"': function() {
+        var actionTarget = function() {
+            return $(":containsExcludeChildren(Use State)").eq(6);
+        };
+        act.click(actionTarget);
+    },
+    '2.Click div "Alex Barrow"': function() {
+        var actionTarget = function() {
+            return $(".from").eq(1);
+        };
+        act.click(actionTarget);
+    },
+    '3.Click span "Diversity Uptake..."': function() {
+        var actionTarget = function() {
+            return $(":containsExcludeChildren(Diversity Uptake of Delegates)").eq(0);
+        };
+        act.click(actionTarget);
+    },
+    '4.Click link "Preview"': function() {
+        var actionTarget = function() {
+            return $(":containsExcludeChildren(Preview)").eq(0);
+        };
+        act.click(actionTarget);
+    },
+    '5.Click button "Save to Documents..."': function() {
+        var actionTarget = function() {
+            return $("#modal\\:ck6w30s18000f3b6rra40iz0e").find(":containsExcludeChildren(Save to Documents)");
+        };
+        act.click(actionTarget);
+    },
+    '6.Click span "Documents"': function() {
+        var actionTarget = function() {
+            return $("#root").find(":containsExcludeChildren(Documents)").eq(0);
+        };
+        act.click(actionTarget);
+    },
+    '7.Click div "Diversity Uptake..."': function() {
+        var actionTarget = function() {
+            return $("[data-testid='document-name'].document-name.ellipsed").eq(0);
+        };
+        act.click(actionTarget);
     }
 };
 
