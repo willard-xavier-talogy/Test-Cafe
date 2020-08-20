2e6c5622ca3580f04c77b955b813e509ebe6d4c3
psiservices-wxavier <56822099+psiservices-wxavier@users.noreply.github.com>
2020-08-20 08:42:16 +0100

    updates
    updating the scripts

--- Auto Test/Apollo/Sending Emails/participants_site_email_-_all_should_pass.test.js
+++ Auto Test/Apollo/Sending Emails/participants_site_email_-_all_should_pass.test.js
@@ -6,7 +6,7 @@
         act.click(":attrRegExp(id:/^ctl\\d+_MainContent_ApolloLabel10$/)");
     },
     '2.Type in input "Username*"': function() {
-        act.type("#txtUsernameForReset", "QAwx4Example4bwx13");
+        act.type("#txtUsernameForReset", "QAwx4Example4bwx4");
     },
     '3.Click input "Email Address*"': function() {
         act.click("#txtEmailAddress");
